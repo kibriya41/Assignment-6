341455f02ba6e34cfd300cfdb2d4d8085e40a592
kibriya41 <kibriyaamit17@gmail.com>
2026-03-30 08:54:05 +0600

    add font and check daisyui, tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import Navbar from './Components/navbar'
 
 function App() {
   
 
   return (
     <>
+      <Navbar/>
      <h1 className='text-5xl text-red-400 text-center'>hello wold</h1>
     </>
   )
